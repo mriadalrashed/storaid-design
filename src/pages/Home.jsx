b00ced020ad395465a6afd8fefcfcca10b0be779
Riad Alrashed <m.riad.alrashed@gmail.com>
2025-10-22 18:29:06 +0200

    Add Hero section component with main heading and background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+
 export default function Home() {
-  return <h1></h1>;
+  return (
+    <>
+      <Hero />
+      
+    </>
+  );
 }
